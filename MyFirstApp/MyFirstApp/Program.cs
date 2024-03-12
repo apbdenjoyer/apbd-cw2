8cae0200694597e683c61a9987b230ae7ed5af40
Franciszek Gozdecki <s27379@pjwstk.edu.pl>
2024-03-12 11:30:15 +0100

    Added GetAverage()

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -4,3 +4,15 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikacja 1");
 Console.WriteLine("Modyfikacja 2");
 Console.WriteLine("Modyfikacja 3");
+
+static double GetAverage(int[] numbers)
+{
+
+    int sum = 0;
+    foreach (var number in numbers)
+    {
+        sum += number;
+    }
+
+    return (double) sum / numbers.Length;
+}
